5a9acb339617fc797eee880a9c6d0d45cf63b38f
Souvik <souvik13109@gmail.com>
2025-03-24 16:05:12 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx"; 
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx"; 
+import Home from "./pages/Home/Home.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
 import Login from "./pages/Login/Login.jsx";
 import Team from "./pages/Team/Team.jsx";
@@ -8,17 +9,30 @@ import Merchandise from "./pages/Merchandise/Merchandise.jsx";
 import Schedule from "./pages/Schedule/Schedule.jsx";
 import Conditions from "./pages/Conditions/Conditions.jsx";
 
+const AppContent = () => {
+  const location = useLocation();
+  const hideNavbar = location.pathname === "/login" || location.pathname === "/signup";
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />} 
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/merchandise" element={<Merchandise />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/terms" element={<Conditions />} />
+      </Routes>
+    </>
+  );
+};
+
 const App = () => (
   <Router>
-    <Routes>
-      <Route path="/" element={<Home />} /> {/* Home Route */}
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/team" element={<Team />} />
-      <Route path="/merchandise" element={<Merchandise />} />
-      <Route path="/schedule" element={<Schedule />} />
-      <Route path="/terms" element={<Conditions />} />
-    </Routes>
+    <AppContent />
   </Router>
 );
 
