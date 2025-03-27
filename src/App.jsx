8ce493d0559652400af69109748271c01a8b76bd
Subhasis112 <mahatosubhasis06@gmail.com>
2025-03-27 23:43:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup/Signup.jsx";
 import Login from "./pages/Login/Login.jsx";
+import ContactUs from "./components/ContactUs/ContactUs.jsx";
 
 const App = () => (
+  <>
+  
   <Router>
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   </Router>
+  <ContactUs/>
+  </>
 );
 
 export default App;
